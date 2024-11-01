487e974f6ea574ebd40037095843d997b071ad61
Pablo Piedrabuena <pablo.piedrabuena8@gmail.com>
2024-11-01 17:18:37 -0300

    GGG

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,9 +1,11 @@
 import '../NavBar/NavBar.css'
+import imgCarrito from './assets/carrito.svg'
 const CartWidget =()=>{
     return (
         <>
             <div className='carrito__nav-ecomerce'>
-                <img src="./src/components/NavBar/assets/carrito.svg" alt="carrito" />
+                {/* <img src="./src/components/NavBar/assets/carrito.svg" alt="carrito" /> */}
+                <img src={imgCarrito} alt="carrito" />
                 <span>1500</span>
             </div>
         </>
