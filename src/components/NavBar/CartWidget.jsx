3bcc989766acf20a9cc5395dd03f4c2449db2be2
Pablo Piedrabuena <pablo.piedrabuena8@gmail.com>
2024-11-01 17:29:21 -0300

    PPP

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,6 +1,6 @@
 import '../NavBar/NavBar.css'
 /* import imgCarrito from './assets/carrito.svg' */
-import imgCarrito from './assets/carrito.svg'
+import imgCarrito from '../assets/carrito.svg'
 const CartWidget =()=>{
     return (
         <>
