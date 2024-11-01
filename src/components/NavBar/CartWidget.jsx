5a6510040f3770498f038f467135a8b446da10fa
Pablo Piedrabuena <pablo.piedrabuena8@gmail.com>
2024-11-01 18:18:15 -0300

    2

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,12 +1,12 @@
 import '../NavBar/NavBar.css'
 /* import imgCarrito from './assets/carrito.svg' */
-import imgCarrito from '/components/NavBar/assets/carrito.svg'
+import imgCarrito2 from '/components/NavBar/assets/carrito.svg'
 const CartWidget =()=>{
     return (
         <>
             <div className='carrito__nav-ecomerce'>
                 {/* <img src="./src/components/NavBar/assets/carrito.svg" alt="carrito" /> */}
-                <img src={imgCarrito} alt="carrito" />
+                <img src={imgCarrito2} alt="carrito" />
                 <span>1500</span>
             </div>
         </>
